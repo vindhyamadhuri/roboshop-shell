e564e422a1494d1f26a6c2a29d244b2d78ca7b5f
vindhyamadhuri <vindhyamadhuri99@gmai.com>
2023-04-23 21:49:06 +0530

    giving path to mongo.repo

--- catalogue.sh
+++ catalogue.sh
@@ -30,7 +30,7 @@ systemctl enable catalogue
 systemctl start catalogue
 
 echo -e "\e[36m>>>>>>>>>>Copy mongodb repo<<<<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[36m>>>>>>>>> install mongodb client<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
