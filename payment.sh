7250ada1409cf7c6e2c1911db5e56c8527935ca6
vindhyamadhuri <vindhyamadhuri99@gmai.com>
2023-04-24 00:41:09 +0530

    payment service 2

--- payment.sh
+++ payment.sh
@@ -1,14 +1,19 @@
 yum install python36 gcc python3-devel -y
+
 useradd roboshop
+
 rm -rf /app
 mkdir /app
+
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+
 cd /app
 unzip /tmp/payment.zip
 
 pip3.6 install -r requirements.txt
 
 cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
+
 systemctl daemon-reload
 systemctl enable payment
 systemctl restart payment
