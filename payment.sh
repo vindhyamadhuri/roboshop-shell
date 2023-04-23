397e2f684c394d1dca5c92d33d4593e9adbeac63
vindhyamadhuri <vindhyamadhuri99@gmai.com>
2023-04-24 00:53:03 +0530

    payment service 3

--- payment.sh
+++ payment.sh
@@ -1,19 +1,27 @@
+echo -e "\e[36m>›››››››› Install Python ‹‹‹‹‹‹<<\e[0m"
 yum install python36 gcc python3-devel -y
 
+echo -e "\e[36m>›››››››> Add Application User ‹‹‹‹‹‹‹‹\e[0m"
 useradd roboshop
 
+echo -e "\e[36m>››››››››> Create App Dir «‹‹‹‹‹<‹\e[0m"
 rm -rf /app
 mkdir /app
 
+echo -e "\e[36m>>›››››>> Download App Content <‹‹‹‹<<<\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 
+echo -e "le[36m>›››››››> Extract App Content <‹‹‹‹‹<‹\e[Om"
 cd /app
 unzip /tmp/payment.zip
 
-pip3.6 install -r requirements.txt
+echo -e "\e[36m>›››››››› Install Dependencies <‹‹<<<<<\e[0m"
+pip3.6 install -r requirements. txt
 
-cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
+echo -e "\e[36m>››››››>›Setup SystemD Service ««‹‹‹‹<<\e[0m"
+cp /home/centos/roboshop-shell/payment.service /etc/system/system/payment.service
 
-systemctl daemon-reload
+echo -e "\e[36m>›››››››> Start Payment Service «‹‹‹‹‹<<\e[0m"
+systemctl deamon-reload
 systemctl enable payment
 systemctl restart payment
