61379c102ba4672ac19ce63f8228e00d72847ce1
vindhyamadhuri <vindhyamadhuri99@gmai.com>
2023-04-23 12:27:52 +0530

    Create frontend script

---
+++ frontend.sh
@@ -0,0 +1,10 @@
+yum install nginx -y
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+## SOME FILE NEEDS TO BE CREATED
+
+systemctl restart nginx
+systemctl enable nginx
