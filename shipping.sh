f1733ce438ff3d2136cf1b86f17fc248f9c29d5a
vindhyamadhuri <vindhyamadhuri99@gmai.com>
2023-04-24 00:01:32 +0530

    shipping service 3

--- shipping.sh
+++ shipping.sh
@@ -1,16 +1,31 @@
+echo -e "\e[36m>›››››››› Install Maven «‹‹‹‹‹‹‹\e[0m"
 yum install maven -y
+
+echo -e "\e[36m>›››››››> Create App User ‹‹‹‹‹‹<‹\e[0m"
 useradd roboshop
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+
+echo -e "\e[36m>›››>››››› Create App Directory <‹‹‹<<<\e[0m"
 rm -rf /app
+mkdir /app
+
+echo -e "\e[36m>>››››››>> Download App Content <‹‹‹‹<<<\e[0m"
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+
+echo -e "\e[36m>›>›››››> Extract App Content ‹‹‹‹‹‹<<\e[0m"
 cd /app
 unzip /tmp/shipping.zip
+
+echo -e "\e[36m>›>>›››>> Download Maven Dependencies <‹‹‹«<<<\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
+echo -e "\e[36m>›››››››› Install MySqL «‹‹‹‹‹<<\e[0m"
 yum install mysql -y
+
+echo -e "\e[36m>›››››››› Load Schema «‹‹‹‹<<<\e[0m"
 mysql -h mysql-dev.vin95.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
-cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+
+echo -e "\e[36m>››>›››››› Start Shipping Service «‹«<‹le[0m"
+systemct1 daemon-reload
+systemct1 enable shipping
+systemct1 restart shipping
