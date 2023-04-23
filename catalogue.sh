075b094be1153f134606e1023f63cbc343f01c4c
vindhyamadhuri <vindhyamadhuri99@gmai.com>
2023-04-23 20:28:12 +0530

    adding print statements to catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -1,15 +1,38 @@
+echo -e "\e[36m>>>>>>>configuring Node.js repos<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>>> Install Nodejs<<<<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[36m>>>>>>>>Add Application user<<<<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>Create application directory<<<<<<<<<<<<<\e[0m"
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>Download app content<<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>>Unzip app content<<<<<<<<<<<<\e[0m"
 unzip /tmp/catalogue.zip
+
+echo -e "\e[36m>>>>>>>>>install nodejs dependencies<<<<<<<<\e[0m"
 npm install
+
+echo -e "\e[36m>>>>>>>>> copy catalogue systemd file<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[36m>>>>>>>>Start catalogue service<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
+
+echo -e "\e[36m>>>>>>>>>>Copy mongodb repo<<<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>>>>> install mongodb client<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
+
+echo -e "\e[36m>>>>>>>>>> Load Schema<<<<<<<<<<<<\e[0m"
 mongo --host mongodb-dev.vin95.online </app/schema/catalogue.js
