d70b3ac3d3117fb5bddb33962fa8820b53909a89
vindhyamadhuri <vindhyamadhuri99@gmai.com>
2023-04-23 22:13:25 +0530

    redis script with color messages

---
+++ redis.sh
@@ -0,0 +1,13 @@
+echo -e "\e[36m>>>>>>> Install redis repos<<<<<<<<\e[0m"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[36m>>>>>>> Install redis<<<<<<<<\e[0m"
+dnf module enable redis:remi-6.2 -y
+yum install redis -y
+
+echo -e "\e[36m>>>>>>> Update redis listen address<<<<<<<<\e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+
+echo -e "\e[36m>>>>>>> Start redis service<<<<<<<<\e[0m"
+systemctl enable redis
+systemctl start redis
