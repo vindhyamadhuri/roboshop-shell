761bf2bbac2747e6e48e11018896aa56ee7517d8
vindhyamadhuri <vindhyamadhuri99@gmai.com>
2023-04-23 23:26:55 +0530

    mysql service

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,8 @@
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
 yum install mysql-community-server -y
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
+
+echo -e "\e[36m>>>>>>>Reset mysql password<<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
