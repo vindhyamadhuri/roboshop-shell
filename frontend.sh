bb8c5aec4f2fa0e0f23db8056b634b907fdba845
vindhyamadhuri <vindhyamadhuri99@gmai.com>
2023-04-24 01:41:17 +0530

    payment service 9

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,15 @@
 yum install nginx -y
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
 rm -rf /usr/share/nginx/html/*
+
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
 cd /usr/share/nginx/html
+
 unzip /tmp/frontend.zip
+
 systemctl restart nginx
+
 systemctl enable nginx
